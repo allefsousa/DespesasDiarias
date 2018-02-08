97171f83b4867bf8c2311fb9597a93dbdfb64d69
Allef Sousa <allefsousa_1@hotmail.com>
2018-02-07 22:29:10 -0200

    Firebase para autenticacao e salvar dados remotos

--- app/src/main/java/br/com/allefdeveloper/despesasmensais/CalendarActivity.java
+++ app/src/main/java/br/com/allefdeveloper/despesasmensais/CalendarActivity.java
@@ -11,13 +11,18 @@ import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import butterknife.BindColor;
+import butterknife.ButterKnife;
+
 public class CalendarActivity extends AppCompatActivity {
+    @BindColor(R.color.colorAccent)
+    int arrow;
 
     private MaterialCalendarView cal;
-    Boolean sim = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
         setContentView(R.layout.activity_calendar);
         cal = (MaterialCalendarView) findViewById(R.id.calendarView);
         cal.setTopbarVisible(true);
@@ -31,6 +36,8 @@ public class CalendarActivity extends AppCompatActivity {
             @Override
             public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
                 Toast.makeText(CalendarActivity.this, "Data Atual" +date, Toast.LENGTH_LONG).show();
+                widget.setArrowColor(arrow);
+                widget.setSelectionColor(arrow);
 
             }
         });
